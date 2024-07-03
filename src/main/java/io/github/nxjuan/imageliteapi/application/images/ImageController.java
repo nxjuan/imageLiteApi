3326c67228eb825ad148a6928c1d666b752feeb7
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-03 15:32:37 -0300

    Configurando services e controllers para receberem imagens

--- src/main/java/io/github/nxjuan/imageliteapi/application/images/ImageController.java
+++ src/main/java/io/github/nxjuan/imageliteapi/application/images/ImageController.java
@@ -1,19 +1,20 @@
 package io.github.nxjuan.imageliteapi.application.images;
 
 import io.github.nxjuan.imageliteapi.domain.entities.Image;
-import io.github.nxjuan.imageliteapi.domain.enums.ImageExtencion;
 import io.github.nxjuan.imageliteapi.domain.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
@@ -22,24 +23,29 @@ import java.io.IOException;
 public class ImageController {
 
     private final ImageService service;
+    private final ImageMapper mapper;
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
-            @RequestParam("tags") String tags
+            @RequestParam("tags") List<String> tags
     ) throws IOException {
         log.info("Imagem Recebida: name: {}, size: {}", file.getName(), file.getSize());
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",", tags ))
-                .size(file.getSize())
-                .extension(ImageExtencion.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageURI = buildImageURL(savedImage);
 
-        service.save(image);
-
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(imageURI).build();
     }
+
+    private URI buildImageURL(Image image){
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build()
+                .toUri();
+    }
+
 }
