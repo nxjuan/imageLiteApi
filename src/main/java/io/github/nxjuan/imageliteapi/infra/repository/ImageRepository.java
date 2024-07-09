d1fadb9147ebc861ab284e328e1c8cae039ecb8d
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-09 05:52:53 -0300

    Repository search method updated: pesquisa com criterio especifico funcionando

--- src/main/java/io/github/nxjuan/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/nxjuan/imageliteapi/infra/repository/ImageRepository.java
@@ -30,7 +30,7 @@ public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecif
 
             spec = spec.and(nameOrTagsLike);
         }
-        return findAll();
+        return findAll(spec);
     }
 
 }
