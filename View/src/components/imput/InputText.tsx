f7d26023d077b57fc26a0bfac2916399f837e048
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-25 11:51:53 -0300

    imput props created

---
+++ View/src/components/imput/InputText.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface InputTextProps{
+    style?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+    placeHolder?: string;
+}
+
+export const InputText: React.FC<InputTextProps> = ( {style, ...rest}: InputTextProps ) => {
+    return (
+        <input type="text" className={`${style} border bg-gray-200 px-3 py-2 rounded-md text-black`} {...rest} />
+    )
+}
