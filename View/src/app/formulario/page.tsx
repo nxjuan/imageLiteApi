b14b23e9c30b1114ab0e3585f99391b1a6d2ae95
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-07-24 21:39:53 -0300

    forms started

--- View/src/app/formulario/page.tsx
+++ View/src/app/formulario/page.tsx
@@ -1,3 +1,10 @@
+import { Template } from '@/components'
+
 export default function Formulario(){
-    return <h1>Formulario</h1>
+    return (
+        <Template>
+            <h1 className="text-gray-600">Formularios</h1>
+        </Template>
+    )
+    
 }
