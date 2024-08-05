bea85fd1e713580d4a638ad0d47e7508525cfd53
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-05 07:18:52 -0300

    Button text color default removed

--- View/src/components/button/Button.tsx
+++ View/src/components/button/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export const Button: React.FC<ButtonProps> = ({onclick, label, style, type} : ButtonProps) => {
     return (
         <>
-            <button className={`${style} text-white px-4 py-2 rounded-lg`} type={type} onClick={onclick}>
+            <button className={`${style} px-4 py-2 rounded-lg`} type={type} onClick={onclick}>
                 { label }
             </button>
         </>
