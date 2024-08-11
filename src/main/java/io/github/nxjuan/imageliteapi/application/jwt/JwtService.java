aace62aea0c6a37300fc44c01818a4e9ecd243c3
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-10 22:32:41 -0300

    Token access implemented

---
+++ src/main/java/io/github/nxjuan/imageliteapi/application/jwt/JwtService.java
@@ -0,0 +1,51 @@
+package io.github.nxjuan.imageliteapi.application.jwt;
+
+import io.github.nxjuan.imageliteapi.domain.AccessToken;
+import io.github.nxjuan.imageliteapi.domain.entities.User;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final SecretKeyGenerator keyGenerator;
+    public AccessToken generateToken(User user){
+
+        var key = keyGenerator.getKey();
+        var expirationDate = generateExpirationDate();
+        var claims = generateTokemClaims(user);
+
+        String token = Jwts
+                .builder()
+                .signWith(key)
+                .subject(user.getEmail())
+                .expiration(expirationDate)
+                .claims(claims)
+                .compact();
+
+        return new AccessToken("");
+    }
+
+    private Date generateExpirationDate(){
+        var expirationMinutes = 60;
+        LocalDateTime now = LocalDateTime.now().plusMinutes(expirationMinutes);
+
+        return Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private Map<String, Object> generateTokemClaims(User user){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("name", user.getName());
+        return claims;
+    }
+
+}
