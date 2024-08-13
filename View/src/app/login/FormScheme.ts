ce276b86a9fedbbc505158da5dbb7f5d648af023
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-13 10:04:46 -0300

    Form updated

--- View/src/app/login/FormScheme.ts
+++ View/src/app/login/FormScheme.ts
@@ -11,7 +11,8 @@ export const validationScheme = Yup.object().shape({
     email: Yup.string().trim().required('Email is required').email('Invalid Email'),
     password: Yup.string().required('Password is required').min(8, 'Password must be have at least 8 characters'),
 
-    passwordMatch: Yup.string().oneOf([Yup.ref('password')], 'Password must match!')
+    passwordMatch: Yup.string().oneOf([Yup.ref('password')], 'Password must match!'),
+    name: Yup.string().trim().required('name is required').min(2, 'Name can\'t be too short')
 })
 
 export const formScheme: LoginForm = {email: '', name: '', password: '', passwordMatch: ''} 
