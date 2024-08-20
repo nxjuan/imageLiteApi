25769836f800d613a0f0b5c4668ebfff28a8fd36
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-20 15:07:04 -0300

    Authentication implemented

--- View/src/app/formulario/page.tsx
+++ View/src/app/formulario/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { InputText, Template, Button, RenderIf, useNotification, FieldError } from '@/components'
+import { InputText, Template, Button, RenderIf, useNotification, FieldError, AuthenticatedPage } from '@/components'
 import Link from 'next/link'
 import { useFormik } from 'formik'
 import { useState } from 'react';
@@ -50,59 +50,61 @@ export default function Formulario(){
     }
 
     return (
-        <Template loading={loading}>
-            <section className='flex flex-col items-center justfy-center my-5'>
-                <h5 className='mt-3 mb-10 text-3xl font-extrabold tracking-tight text-translate-900 text-gray-700'>Nova Imagem</h5>
-                <form onSubmit={formik.handleSubmit}>
-                    <div className='grid grid-cols-1'>
-                        <label className='block text-sm font-medium leading-6 text-gray-600'>Name: *</label>
-                        <InputText id="name" onChange={formik.handleChange} placeholder="type the image's name" value={formik.values.name} />
-                        <FieldError error={formik.errors.name} />
-                    </div>
+        <AuthenticatedPage>
+            <Template loading={loading}>
+                <section className='flex flex-col items-center justfy-center my-5'>
+                    <h5 className='mt-3 mb-10 text-3xl font-extrabold tracking-tight text-translate-900 text-gray-700'>Nova Imagem</h5>
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className='grid grid-cols-1'>
+                            <label className='block text-sm font-medium leading-6 text-gray-600'>Name: *</label>
+                            <InputText id="name" onChange={formik.handleChange} placeholder="type the image's name" value={formik.values.name} />
+                            <FieldError error={formik.errors.name} />
+                        </div>
 
-                    <div className='mt-5 grid grid-cols-1'>
-                        <label className='block text-sm font-medium leading-6 text-gray-600'>Tags: *</label>
-                        <InputText id='tags' onChange={formik.handleChange} placeholder="type the tags comma separated" value={formik.values.tags} />
-                        <FieldError error={formik.errors.tags} />
-                    </div>
+                        <div className='mt-5 grid grid-cols-1'>
+                            <label className='block text-sm font-medium leading-6 text-gray-600'>Tags: *</label>
+                            <InputText id='tags' onChange={formik.handleChange} placeholder="type the tags comma separated" value={formik.values.tags} />
+                            <FieldError error={formik.errors.tags} />
+                        </div>
 
-                    <div className='mt-5 grid grid-cols-1'>
-                        <label className='block text-sm font-medium leading-6 text-gray-600'>Image: *</label>
-                        <FieldError error={formik.errors.file} />
-                        <div className='mt-2 flex justify-center rounded-lg  border border-dashed border-gray-900/25 px-6 py-10'>
-                            <div className='text-center'>
+                        <div className='mt-5 grid grid-cols-1'>
+                            <label className='block text-sm font-medium leading-6 text-gray-600'>Image: *</label>
+                            <FieldError error={formik.errors.file} />
+                            <div className='mt-2 flex justify-center rounded-lg  border border-dashed border-gray-900/25 px-6 py-10'>
+                                <div className='text-center'>
 
-                                <RenderIf condition={!imagePreview}>
-                                    <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-                                        <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
-                                    </svg>
-                                </RenderIf>
-                                
-                                <div className='mt-4 flex text-sm leading-6 text-gray-600'>
-                                    <label className='relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600'>
-                                        <RenderIf condition={!imagePreview}>
-                                            <span className=''>
-                                                Click to upload
-                                            </span>
-                                        </RenderIf>   
-                                        <RenderIf condition={!!imagePreview}>
-                                            <img src={imagePreview} width={250} className='rounded=md' />
-                                        </RenderIf>                                     
-                                        <input type='file' onChange={onFIleUpload} className='sr-only'/>
-                                    </label>
+                                    <RenderIf condition={!imagePreview}>
+                                        <svg className="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                            <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+                                        </svg>
+                                    </RenderIf>
                                     
+                                    <div className='mt-4 flex text-sm leading-6 text-gray-600'>
+                                        <label className='relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600'>
+                                            <RenderIf condition={!imagePreview}>
+                                                <span className=''>
+                                                    Click to upload
+                                                </span>
+                                            </RenderIf>   
+                                            <RenderIf condition={!!imagePreview}>
+                                                <img src={imagePreview} width={250} className='rounded=md' />
+                                            </RenderIf>                                     
+                                            <input type='file' onChange={onFIleUpload} className='sr-only'/>
+                                        </label>
+                                        
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className='mt-6 flex items-center justify-end gap-x-6'>
-                        <Button style='bg-blue-600 hover:bg-blue-900' label='save' type='submit' />
-                        <Link href='/galeria'>
-                            <Button style='bg-white border border-red-500 hover:text-white hover:bg-red-500 text-gray-900' label='cancel' type='button' />
-                        </Link>
-                    </div>
-                </form>
-            </section>            
-        </Template>
+                        <div className='mt-6 flex items-center justify-end gap-x-6'>
+                            <Button style='bg-blue-600 hover:bg-blue-900' label='save' type='submit' />
+                            <Link href='/galeria'>
+                                <Button style='bg-white border border-red-500 hover:text-white hover:bg-red-500 text-gray-900' label='cancel' type='button' />
+                            </Link>
+                        </div>
+                    </form>
+                </section>            
+            </Template>
+        </AuthenticatedPage>
     )    
 }
--- View/src/app/galeria/page.tsx
+++ View/src/app/galeria/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Template, ImageCard, Button, InputText, useNotification } from '@/components/'
+import { Template, ImageCard, Button, InputText, useNotification, AuthenticatedPage } from '@/components/'
 import { use, useState } from 'react'
 import { useImageService } from '@/resources/image/image.service'
 import { Image } from '@/resources/image/image.resource';
@@ -45,32 +45,34 @@ export default function GaleriaPage(){
     }
 
     return (
-        <Template loading={loading}>
-            <section className='flex flex-col items-center justfy-center my-5'>
-                <div className='flex space-x-4'>
-                    <InputText onChange={event => setQuery(event.target.value)} placeholder='Digitar'/>
+        <AuthenticatedPage>
+            <Template loading={loading}>
+                <section className='flex flex-col items-center justfy-center my-5'>
+                    <div className='flex space-x-4'>
+                        <InputText onChange={event => setQuery(event.target.value)} placeholder='Digitar'/>
 
-                    <select onChange={event => setExtension(event.target.value)} name="" id="" className='border bg-gray-200 px-4 py-2 rounded-md text-black'>
-                        <option value="">All Formats</option>
-                        <option value="JPEG">JPEG</option>
-                        <option value="PNG">PNG</option>
-                        <option value="GIF">Gif</option>
-                    </select>
+                        <select onChange={event => setExtension(event.target.value)} name="" id="" className='border bg-gray-200 px-4 py-2 rounded-md text-black'>
+                            <option value="">All Formats</option>
+                            <option value="JPEG">JPEG</option>
+                            <option value="PNG">PNG</option>
+                            <option value="GIF">Gif</option>
+                        </select>
 
-                    <Button style="bg-blue-500 hover:bg-blue-900" label='Search' onclick={searchImages}/>
-                    
-                    <Link href="/formulario">
-                        <Button style='bg-blue-700 hover:bg-blue-900' label='Add New'/>
-                    </Link>
-                    
-                </div>
-            </section>
-    
-            <section className='grid grid-cols-4 gap-8'>
-                {
-                    renderImageCards()
-                }
-            </section>               
-        </Template>        
+                        <Button style="bg-blue-500 hover:bg-blue-900" label='Search' onclick={searchImages}/>
+                        
+                        <Link href="/formulario">
+                            <Button style='bg-blue-700 hover:bg-blue-900' label='Add New'/>
+                        </Link>
+                        
+                    </div>
+                </section>
+        
+                <section className='grid grid-cols-4 gap-8'>
+                    {
+                        renderImageCards()
+                    }
+                </section>               
+            </Template>    
+        </AuthenticatedPage>    
     )
 }
