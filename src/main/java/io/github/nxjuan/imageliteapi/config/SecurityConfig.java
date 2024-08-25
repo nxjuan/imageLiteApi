94f6ff9819b27128bf432b392ae5356f367da6cd
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-24 21:31:23 -0300

    Implementing filter Jwt

--- src/main/java/io/github/nxjuan/imageliteapi/config/SecurityConfig.java
+++ src/main/java/io/github/nxjuan/imageliteapi/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package io.github.nxjuan.imageliteapi.config;
 
+import io.github.nxjuan.imageliteapi.application.jwt.JwtService;
+import io.github.nxjuan.imageliteapi.config.filter.JwtFilter;
+import io.github.nxjuan.imageliteapi.domain.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,22 +21,25 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     @Bean
+    public JwtFilter jwtFilter(JwtService jwtService, UserService userService){
+        return new JwtFilter(jwtService, userService);
+    }
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configure(http))
                 .authorizeHttpRequests(auth -> {
-
                     auth.requestMatchers("/v1/users/**").permitAll();
-
                     auth.anyRequest().authenticated();
-
                 })
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
