072a6c47b3b419d5448087bdb0381cec08f340a4
Juan Nogueira <juannogueiraxavier@gmail.com>
2024-08-27 14:54:52 -0300

    'not found' page created

---
+++ View/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Template } from "@/components";
+
+export default function NotFoundPage(){
+    return(
+        <Template>
+            <section className="text-gray-900 text-x1 text-center">
+                <h1>Page not found!</h1>
+            </section>
+        </Template>
+    )
+}
